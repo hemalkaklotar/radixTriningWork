50c40a0469c1eb59311f940b91c7de2ee8e4476f
hemalkaklotar <hemalkaklotar13@gmail.com>
2022-08-22 19:31:18 +0530

    added task for day 3 and for in supreme project

--- suprimeProject/script.js
+++ suprimeProject/script.js
@@ -1,25 +1,9 @@
-function showTabsWorkType(id){
+function myFunction(id){
     const element = document.querySelector(id);
-    console.log(id);
-    console.log("element ->",element);
-     element.classList.toggle("show");
-     console.log("class List",element.classList);
-
-    // element.addEventListener("click",changeBehaviour);
-    // console.log("class List",element.classList);
-    // function changeBehaviour(){
-    //     console.log("added event Listener");
-    //     element.classList.add("show");
-    // }
-
-    // console.log("assignment cllicked ?")
-    // const element = document.querySelector(id);
-    // element.classList.toggle("show");
-}
-function showTabsWorkType(id){
-    const element = document.querySelector(id);
-    console.log(id);
-    console.log("element ->",element);
-     element.classList.toggle("show");
-     console.log("class List",element.classList);
-}
+    element.classList.toggle("hide");
+ }
+//  function showWorkSkeleton(id){
+//     const element = document.querySelector(id);
+//     element.classList.toggle("hideWorkSkeleton");
+//  }
+ 
